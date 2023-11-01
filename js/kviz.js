f57c83a90ebc1c204e109d8942cc902b8f2e17c3
JuhaszZs <juhaszzs@kkszki.hu>
2023-11-01 16:09:37 +0100

    JS fist steps done, but not working.

--- js/kviz.js
+++ js/kviz.js
@@ -1,16 +1,44 @@
 function checkAnswer() {
-    const correctAnswer = "2";
-    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    const correctAnswer = {
+        question1:"2",
+        question2:"2",
+        question3:"2",
+        question4:"1",
+        question5:"3",
+        question6:"2",
+        question7:"3",
+        question8:"3",
+        question9:"3",
+        question10:"1"
+    }
+    const userAnswers = {
+        question1: document.querySelector('input[name="question1"]:checked').value,
+        question2: document.querySelector('input[name="question2"]:checked').value,
+        question3: document.querySelector('input[name="question3"]:checked').value,
+        question4: document.querySelector('input[name="question4"]:checked').value,
+        question5: document.querySelector('input[name="question5"]:checked').value,
+        question6: document.querySelector('input[name="question6"]:checked').value,
+        question7: document.querySelector('input[name="question7"]:checked').value,
+        question8: document.querySelector('input[name="question8"]:checked').value,
+        question9: document.querySelector('input[name="question9"]:checked').value,
+        question10: document.querySelector('input[name="question10"]:checked').value,
+    };
 
-     if (selectedAnswer) {
-                if (selectedAnswer.value === correctAnswer) {
-                    document.getElementById("result").textContent = "Helyes válasz!";
-                } else {
-                    document.getElementById("result").textContent = "Rossz válasz. Próbáld újra.";
-                }
-            } 
-            else {
-                document.getElementById("result").textContent = "Válassz egy választ!";
-             }
-             
-              }
+    if (
+        userAnswers.question1 === correctAnswers.question1 &&
+        userAnswers.question2 === correctAnswers.question2 &&
+        userAnswers.question3 === correctAnswers.question3 &&
+        userAnswers.question4 === correctAnswers.question4 &&
+        userAnswers.question5 === correctAnswers.question5 &&
+        userAnswers.question6 === correctAnswers.question6 &&
+        userAnswers.question7 === correctAnswers.question6 &&
+        userAnswers.question8 === correctAnswers.question6 &&
+        userAnswers.question9 === correctAnswers.question6 &&
+        userAnswers.question10 === correctAnswers.question6 &&
+    ) {
+        document.getElementById("result").textContent = "Gratulálok, minden válasz helyes!";
+    } else {
+        document.getElementById("result").textContent = "Nem minden válasz helyes, próbáld újra.";
+    }
+}
+              
